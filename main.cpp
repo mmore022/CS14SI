b1a8b56a081a3f0e3488e11078b812ff3a8313d2
mmore022 <mmore022@ucr.edu>
2015-04-08 21:45:10 +0000

    more comments

--- main.cpp
+++ main.cpp
@@ -11,3 +11,16 @@ int main()
 
     return 0;
 }
+
+// cd
+// mkdir Name -to make a new folder
+// cd Name
+// git init (makes a new repository)
+// touch main.cpp
+// git status 
+// git add [files] (always add files before you commit it)
+// git commit -m "Added Hello World!"
+// git log (to see all the previous stuff)
+// touch README.md
+// git push origin 
+// git pull origin
